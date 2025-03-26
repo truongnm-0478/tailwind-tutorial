b6092212c9cd04d2a1ed01908e6819eb43bbf8c7
truongnm-0478 <ngo.mau.truong@sun-asterisk.com>
2025-03-27 06:30:40 +0700

    part 2,3,4,5

--- part_01/tailwind.config.js
+++ part_01/tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './**/*.html',
+        './**/*.{js,jsx,ts,tsx,vue}',
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            spacing: {
+                '35': '35px'
+            }
+        },
+        colors: {
+            gray33: '#333',
+            orangefa: '#ffa400'
+        }
+    },
+    plugins: [],
 }
 
